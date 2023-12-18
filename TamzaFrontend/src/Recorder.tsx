e09bfaeae4523d8e54abaf4df29d20446410529e
Khagan-Massin <massin.elotmani@gmail.com>
2023-12-18 13:39:08 +0100

    started on react

---
+++ TamzaFrontend/src/Recorder.tsx
@@ -0,0 +1,24 @@
+
+import { useState } from 'react'
+ 
+function Recorder() {
+  const [recordedChunks, setRecordedChunks] = useState()
+  const [recording, setRecording] = useState(false)
+  const [recorder, setRecorder] = useState()
+ 
+
+  startRecording = () => {
+
+
+  }
+    
+
+
+
+  return (
+    <>
+       
+    </>
+  )
+}
+ 
