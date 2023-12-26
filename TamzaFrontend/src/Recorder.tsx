4eea05c39f7b23c9b9e1c055fe755b08d8760309
Khagan-Massin <massin.elotmani@gmail.com>
2023-12-26 12:16:06 +0100

    ok so lets simplyfy

--- TamzaFrontend/src/Recorder.tsx
+++ TamzaFrontend/src/Recorder.tsx
@@ -1,26 +1,35 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useState } from 'react'
 
 export function Recorder() {
   const [recordedChunks, setRecordedChunks] = useState([] as Blob[])
   const [isRecording, setIsRecording] = useState(false)
-  const [recorder, setRecorder] = useState<MediaRecorder| null>(null); 
+  const [recorder, setRecorder] = useState<MediaRecorder | null>(null);
 
   const audioPlayerRef = useRef<HTMLAudioElement>(null);
 
+  // eslint-disable-next-line prefer-const
+  let audioRecorder: MediaRecorder | null = null;
+  let chunks: Blob[] = [];
+
+
   function hasRecording(): boolean {
-    return recordedChunks.length > 0
+
+    const bool = recordedChunks.length > 0
+
+    return bool
   }
 
   function handleStopStart(): void {
 
     if (isRecording) {
       stopRecording()
+
     } else {
       startRecording()
     }
-    console.log('After: isRecording: ' + isRecording)
+
   }
 
   function startRecording() {
@@ -29,109 +38,138 @@ export function Recorder() {
       throw new Error("Already recording")
     }
 
-    setIsRecording(true)
+
     setRecordedChunks([])
 
-    // TODO: start recording
-
-
     navigator.mediaDevices.getUserMedia({ audio: true })
-    .then(stream => {
-      setRecorder(new MediaRecorder(stream))
-      recorder?.addEventListener('dataavailable', (e) => {
-        if (e.data.size > 0) {
-          setRecordedChunks((prev: Blob[]) => [...prev, e.data])
+      .then(stream => {
+
+        audioRecorder = new MediaRecorder(stream)
+        audioRecorder.start()
+
+
+        audioRecorder.ondataavailable = (e) => {
+
+
+          console.log('data available')
+          
+            setRecordedChunks((prev) => [...prev, e.data]);
+            console.log(recordedChunks)
+          
+
+        };
+
+        audioRecorder.onstop = () => {
+          console.log('stop')
+          console.log(audioRecorder.state)
+          console.log("recorder stopped");
+          saveAudio()
         }
+
+
+        setIsRecording(true)
+        setRecorder(audioRecorder)
+
+      })
+      .catch(err => {
+        console.log('error getting microphone', err);
       });
 
-      recorder?.addEventListener('stop', () => {
-        console.log('stop event')
-        activateAudioPlayer()
-      });
-    });
-    
- 
+
+
+
+
+  }
+
+  function activateAudioPlayer(): void {
+    const file = saveAudio()
+    if (audioPlayerRef.current) {
+      console.log('audioPlayerRef: ', audioPlayerRef.current)
+      audioPlayerRef.current.src = file
+      return
+    }
+    throw new Error("audioPlayerRef is null")
   }
 
   function stopRecording() {
 
-    if (!isRecording) {
-      throw new Error("Not recording")
+    if (recorder && recorder.state === 'recording') {
+
+      recorder.stop()
+      setIsRecording(false)
+      return
     }
-
-    setIsRecording(false)
-
-    recorder?.stop();
-
+    throw new Error("audioRecorder is null or not recording")
     // if you want something to happen after recording stops 
     // put it in the stop event listener not here
   }
 
   function saveAudio(): string {
+
+    console.log('Saving audio with chunks:');
+
+    console.log();
+
     const blob: Blob = new Blob(recordedChunks, { type: 'audio/mp3' });
 
     const soundfile: string = URL.createObjectURL(blob)
 
+
+    console.log(soundfile)
+
     return soundfile
   }
 
-    function activateAudioPlayer(): void {
-      const file = saveAudio()
-      audioPlayerRef.current!.src = file
-       
+  function downloadAudio() {
+    const file = saveAudio()
+
+    const a = document.createElement('a')
+    a.href = file
+    a.download = 'audio.mp3'
+
+    document.body.appendChild(a)
+
+    a.click()
+
+    document.body.removeChild(a)
+  }
+
+  function restartRecording() {
+
+    if (isRecording) {
+      throw new Error("Already recording")
     }
 
-    function downloadAudio() {
-      const file = saveAudio()
-
-      const a = document.createElement('a')
-      a.href = file
-      a.download = 'audio.mp3'
-
-      document.body.appendChild(a)
-
-      a.click()
-
-      document.body.removeChild(a)
+    if (!recorder) {
+      throw new Error("recorder is null")
     }
 
-    function restartRecording() {
-
-  
-
-     
-
-      setIsRecording(false)
-      setRecordedChunks([])
-
-      
-
-
- 
-
-    }
-
-
-    const html = (
-      <div>
-        <button onClick={handleStopStart}>{isRecording ? 'Stop' : 'Start'}</button>
-        <button onClick={restartRecording}>Restart</button>
-        <button onClick={activateAudioPlayer}>Play</button>
-        <button onClick={downloadAudio}>Download</button>
-        <audio ref={audioPlayerRef} controls />
-      </div>
-    )
-
-    
-
-
-
-
-
-    return html
+    recorder.start()
+    setIsRecording(true)
 
 
   }
+
+
+  return (
+    <>
+      <button onClick={handleStopStart}>{isRecording ? 'Stop' : 'Start'}</button>
+
+      {hasRecording() &&
+        <>
+          <button onClick={restartRecording}>Restart</button>
+          <button onClick={downloadAudio}>Download</button>
+        </>
+      }
+
+      <br />
+
+      <audio ref={audioPlayerRef} controls />
+    </>
+  )
+
+
+}
 
 
 
